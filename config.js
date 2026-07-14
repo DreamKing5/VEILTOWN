a6074f83f25c241d8d90bc46379c7238bee60b46
DreamKing5 <101962066+DreamKing5@users.noreply.github.com>
2026-07-14 10:01:25 +0900

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.VEIL_CONFIG = {
-  supabaseUrl: "https://uuqylaqjipzjxsvvkixv.supabase.co/rest/v1/",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cXlsYXFqaXB6anhzdnZraXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5Mzc0OTUsImV4cCI6MjA5OTUxMzQ5NX0.Hv2nCHtz-CshrtpDdl0xjYzOeSGZY-vicY7s82sCk94"
-};
